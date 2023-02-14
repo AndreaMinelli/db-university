85d3684e6f7cc214bd57d6ce135de7beb42a7060
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-14 16:29:37 +0100

    Completata prima query group

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -61,6 +61,9 @@ WHERE `phone` IS NULL;
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
+SELECT COUNT(*) AS 'numero iscritti', YEAR(`enrolment_date`) as `year`  
+FROM `students`
+GROUP BY `year`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
