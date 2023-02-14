220c90dc1c99633fbc4c22bb75bbb8e05d14e460
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-14 16:14:31 +0100

    Completata terza query

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -14,6 +14,10 @@ WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
+SELECT `name`, `surname`, `date_of_birth`, (YEAR(CURDATE()) - YEAR(`date_of_birth`)) AS `age`
+FROM `students`
+WHERE (YEAR(CURDATE()) - YEAR(`date_of_birth`)) > 30;
+
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
