63d5f765ea0a345894808a62251f11046fba3f0a
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-15 17:27:45 +0100

    Completata query bonus

--- join_query.sql
+++ join_query.sql
@@ -62,3 +62,15 @@ JOIN `departments`
 ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Matematica';
 
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
+-- superare ciascuno dei suoi esami
+
+SELECT `students`.`name`, `students`.`surname`, `exam_student`.`exam_id`, `exam_student`.`vote`, IF(`exam_student`.`vote` >= 18, 1, 0) AS 'attempts'
+FROM `students`
+JOIN `exam_student`
+ON `students`.`id` = `exam_student`.`student_id`
+JOIN `exams`
+ON `exams`.`id` = `exam_student`.`exam_id`
+JOIN `courses`
+ON `courses`.`id` = `exams`.`course_id`
+GROUP BY `students`.`id`, `exam_student`.`exam_id`;
